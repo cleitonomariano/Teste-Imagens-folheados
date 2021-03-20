5b6e7db288bee77d52e75d62a6d872a4b94295ab
Cleiton Mariano <cleitonomariano@gmail.com>
2021-03-19 21:29:36 -0300

    feature/5-create-container-and-content

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -15,6 +15,13 @@ export default createGlobalStyle`
   body, input, button {
     font: 16px Roboto, sans-serif
   }
+  button {
+    cursor: pointer
+  }
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 500;
+    color: #008396
+  }
   #root {
     display: flex;
     max-width: 960px;
